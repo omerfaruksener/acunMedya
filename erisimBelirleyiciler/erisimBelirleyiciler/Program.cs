2663f3ddb8ca29de87e3118ab2e7221f154153af
Ömer Faruk Şener <111159936+omerfaruksener@users.noreply.github.com>
2025-03-14 15:35:18 +0300

    Add files via upload

---
+++ erisimBelirleyiciler/erisimBelirleyiciler/Program.cs
@@ -0,0 +1,15 @@
+﻿using erisimBelirleyiciler;
+
+User user1 = new User("aeaeae@gmail.com");
+user1.setYas(18);
+user1.setAdSoyad("Ahmet Hakan");
+User user2 = new User("aeaeae1@gmail.com");
+user2.setYas(30);
+user2.setAdSoyad("Mehmet Gürel");
+User user3 = new User("aeaeae2@gmail.com");
+user3.setYas(21);
+user3.setAdSoyad("Binnur Akan");
+
+user1.BilgileriGoster();
+user2.BilgileriGoster();
+user3.BilgileriGoster();
---
+++ erisimBelirleyiciler/erisimBelirleyiciler/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erisimBelirleyiciler
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string email;
+
+        public User(string email)
+        {
+            this.email = email;
+        }
+
+        public string getAdSoyad()
+        {
+            return adSoyad;
+        }
+        public void setAdSoyad(string adSoyad)
+        {
+            this.adSoyad = adSoyad;
+        }
+
+        public int getYas() 
+        {
+             return yas;
+        }
+        public void setYas(int yas)
+        {
+            this.yas = yas;
+        }
+
+
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Ad Soyad: " + this.adSoyad);
+            Console.WriteLine("Yaş: " + this.yas);
+            Console.WriteLine("Email: " + this.email);
+        }
+    }
+}
