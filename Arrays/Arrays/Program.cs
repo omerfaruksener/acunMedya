4e150030223ffe84b629ac7d9cd7448d2e1e6d18
Ömer Faruk Şener <111159936+omerfaruksener@users.noreply.github.com>
2025-03-20 22:52:47 +0300

    Add files via upload

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr= new int[5];
+for(int i=0; i<5; i++)
+{
+    int a=int.Parse(Console.ReadLine()) ;
+    arr[i] = a;
+}
+int toplam = 0;
+for (int i = 0; i < 5; i++)
+{
+     toplam += arr[i];
+    Console.WriteLine(toplam);
+    if(i == 4)
+    {
+        Console.WriteLine(toplam / 5);
+    }
+}
