dcbe4116f9e5128d40ac49163c5e8c7ad6541d71
Ömer Faruk Şener <111159936+omerfaruksener@users.noreply.github.com>
2025-03-20 21:26:12 +0300

    Add files via upload

---
+++ Calculator/Calculator/Calculate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public void carp(int x, int y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + x*y);
+        } 
+        public void carp(double x, double y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + x * y);
+        }
+
+        public void cıkart(int x, int y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + (x - y));
+        }
+        public void cıkart(double x, double y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + (x - y));
+        }
+
+        public void bol(int x, int y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("İşlem Sonucu: DNE");
+            }
+            else
+            {
+                Console.WriteLine("İşlem Sonucu: " + (x / y));
+            }
+            
+        }
+
+        public void topla(int x, int y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + (x + y));
+        }
+        public void topla(double x, double y)
+        {
+            Console.WriteLine("İşlem Sonucu: " + (x + y));
+        }
+
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Calculator;
+
+Calculate calculate = new Calculate();
+Console.WriteLine("Lütfen Bir sayı giriniz: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Lütfen ikinci sayıyı giriniz: ");
+int b = int.Parse(Console.ReadLine());
+
+calculate.topla(a, b);
+calculate.cıkart(a, b);
+calculate.carp(a, b);
+calculate.bol(a, b);
