c4dec3f71eb5394b384bd99e5d85b00bb9735057
Ömer Faruk Şener <111159936+omerfaruksener@users.noreply.github.com>
2025-03-25 17:19:10 +0300

    Add files via upload

---
+++ GenericList/GenericList/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Car
+    {
+        public string marka;
+        public string model;
+        public double fuelConsumption;
+        public double totalDistance;
+
+        public Car (string marka, string model, double fuelConsumption, double totalDistance)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.fuelConsumption = fuelConsumption;
+            this.totalDistance = totalDistance;
+        }
+            
+        public void Listele()
+        {
+            double toplamYakit = (totalDistance / 100) * fuelConsumption;
+
+            Console.WriteLine($"Marka:{marka} , Model: {model}, 100 km'de Yaktığı Yakıt: {fuelConsumption}, Toplam Yakıt Tüketimi: {toplamYakit}");
+        }
+    }
+}
---
+++ GenericList/GenericList/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericList;
+
+List<Car> cars = new List<Car>
+{
+    new Car("Toyota", "Corolla", 6.5, 400),
+                new Car("BMW", "X5", 9.0, 400),
+                new Car("Mercedes", "C200", 8.2, 400)
+};
+
+
+foreach(var car in cars)
+{
+    car.Listele();
+}
